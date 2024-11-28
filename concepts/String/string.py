30af1b7009c4651c3f67085105ef4f33a3b9353d
SaAbdullah131 <cse.asabdullah@gmail.com>
2024-11-29 02:05:19 +0600

    String exploring

---
+++ concepts/String/string.py
@@ -0,0 +1,11 @@
+#string is a sequence of characters
+#string is immutable
+#string is an object
+
+#single quotes string 
+desh = 'I am Bangladeshi. He Said "We are in a mess"'
+print(desh)
+
+#double quotes string
+desh2 = "I am Bangladeshi. He Said 'We are in a mess'"
+print(desh2)
--- list_comprehension.py
+++ list_comprehension.py
@@ -1,2 +1,3 @@
 square_list=[[i**2 for i in range(10) if i%2==0] ,[j**3 for j in range(10) if j%2!=0]]#this is list comprehension
 print(square_list)
+#
